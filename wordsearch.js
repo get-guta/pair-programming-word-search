9c6239148ff01057d5050c3cb09c30038c98fc92
getenesh <getenesh.guta@gmail.com>
2023-03-03 21:31:28 -0500

    word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+let result = false;
+const transpose = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < i; j++) {
+            const tmp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = tmp;
+        };
     }
+    return arr;
 }
 
-module.exports = wordSearch
+const horizontalSearch = function (letters, word) {
+    let isExist = false;
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            isExist = true
+
+        }
+    }
+    return isExist;
+
+
+}
+
+const wordSearch = (letters, word) => {
+    if (!letters.length) {
+        return false
+    } else {
+        result = horizontalSearch(letters, word);
+        if (!result) {
+            letters = transpose(letters);
+            result = horizontalSearch(letters, word);
+        }
+
+    }
+return result;
+
+}
+
+module.exports = {wordSearch, transpose}
